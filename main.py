803fa597ec81437d9391697c773e8804dda39075
Nadia <Gamesitfo@gmail.com>
2021-09-25 18:24:15 -0500

    Basic Quote Command and Bot Rename

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import discord, os, sys
+import discord, os, sys, sqlite3, time, datetime, random
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from dotenv import load_dotenv
-from dotenv.compat import to_env
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix="aow!", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="bt", intents=discord.Intents.all())
+slash = SlashCommand(bot)
+guild_ids = [781590063653191701, 456602312920530945, 621888002804678656]
+mafia_id = [781590063653191701, 456602312920530945]
 
-slash = SlashCommand(bot, sync_commands=True)
-guild_ids = [781590063653191701]
+conn = sqlite3.connect('quotes.db')
+c = conn.cursor()
+
 
 @bot.event
 async def on_ready():
@@ -20,16 +23,24 @@ async def on_ready():
     print(f"{bot.user.name} is in {str(guild_count)} servers.")
     print(f"{bot.user.name}\'s id is {bot.user.id}")
 
-@slash.slash(name="ping", guild_ids=guild_ids)
+
+@slash.slash(name="ping",
+             description="Checks the ping to the bot",
+             guild_ids=guild_ids)
 async def _ping(ctx):
     await ctx.send(f"Pong! ({round(bot.latency*1000)}ms)")
 
+# region Mafia-specific
+@slash.slash(name="quote",
+             description="Gives a random quote from the Art of War",
+             guild_ids=mafia_id)
+async def _quote(ctx):
+    c.execute('SELECT * FROM QUOTES ORDER BY RANDOM() LIMIT 1')
+    data = c.fetchall()
+    for row in data:
+        await ctx.send(row[0])
 
-@slash.slash(name="",
-             description="",
-             guild_ids=guild_ids
-             )
-async def _quote(ctx, msg):
-    await ctx.send("Hello, world!")
+    
+# endregion
 
 bot.run(TOKEN)
